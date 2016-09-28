d4ed43e0566fe7e795abd8b37456cb9ce46d28cd
robrecht <robrechtdaems@msn.com>
2016-09-28 12:25:10 +0200

    udpclient

--- src/main/java/prac/ivanrobrecht/sessie2/UDPClient.java
+++ src/main/java/prac/ivanrobrecht/sessie2/UDPClient.java
@@ -10,14 +10,14 @@ public class UDPClient {
     {
         BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
         DatagramSocket clientSocket = new DatagramSocket();
-        InetAddress IPAddress = InetAddress.getByName("localhost");
+        InetAddress IPAddress = InetAddress.getByName("192.168.0.2");
         byte[] sendData = new byte[1024];
         byte[] receiveData = new byte[1024];
 
-        String sentence = inFromUser.readLine();
+        String sentence = "test.txt";
         sendData = sentence.getBytes();
 
-        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 1366);
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 10001);
         clientSocket.send(sendPacket);
         DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
         clientSocket.receive(receivePacket);
