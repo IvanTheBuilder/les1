f1f9271f2f7b54c5206af8867dc323871a294476
robrecht <robrechtdaems@msn.com>
2016-09-28 11:43:20 +0200

    udpclient

--- src/main/java/prac/ivanrobrecht/sessie2/UDPClient.java
+++ src/main/java/prac/ivanrobrecht/sessie2/UDPClient.java
@@ -1,7 +1,30 @@
 package prac.ivanrobrecht.sessie2;
+import java.io.*;
+import java.net.*;
 
 /**
  * Created by Ivan on 28/09/2016.
  */
 public class UDPClient {
+    public static void main(String args[]) throws Exception
+    {
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+        DatagramSocket clientSocket = new DatagramSocket();
+        InetAddress IPAddress = InetAddress.getByName("localhost");
+        byte[] sendData = new byte[1024];
+        byte[] receiveData = new byte[1024];
+
+        String sentence = inFromUser.readLine();
+        sendData = sentence.getBytes();
+
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 1366);
+        clientSocket.send(sendPacket);
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+        clientSocket.receive(receivePacket);
+
+        String receivedSentence = new String(receivePacket.getData());
+        FileOutputStream fileOutputStream = new FileOutputStream("testUDP.txt");
+        fileOutputStream.write(receivedSentence.getBytes());
+        clientSocket.close();
+    }
 }
