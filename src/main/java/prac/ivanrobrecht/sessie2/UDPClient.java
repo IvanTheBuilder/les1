1ef121aace66bb9dda293199662907b872ad6ef0
robrecht <robrechtdaems@msn.com>
2016-09-28 12:45:18 +0200

    udpclient

--- src/main/java/prac/ivanrobrecht/sessie2/UDPClient.java
+++ src/main/java/prac/ivanrobrecht/sessie2/UDPClient.java
@@ -1,6 +1,10 @@
 package prac.ivanrobrecht.sessie2;
+import jdk.internal.util.xml.impl.Input;
+
 import java.io.*;
 import java.net.*;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
 
 /**
  * Created by Ivan on 28/09/2016.
@@ -23,6 +27,21 @@ public class UDPClient {
         clientSocket.receive(receivePacket);
 
         String receivedSentence = new String(receivePacket.getData());
+        String hash = receivedSentence;
+
+        receivePacket = new DatagramPacket(receiveData, receiveData.length);
+        clientSocket.receive(receivePacket);
+
+        receivedSentence = new String(receivePacket.getData());
+
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        byte[] digest;
+        InputStream stream = new ByteArrayInputStream(receivedSentence.getBytes());
+        DigestInputStream dis = new DigestInputStream(stream , md);
+        digest = md.digest();
+        if(digest.equals(hash))
+            System.out.println("hash klopt");
+
         FileOutputStream fileOutputStream = new FileOutputStream("testUDP.txt");
         fileOutputStream.write(receivedSentence.getBytes());
         clientSocket.close();
